7fab263772f17dd310107c5064314c44b0085a09
John S <johnc.sime@gmail.com>
2021-12-14 18:47:40 -0500

    Got the site to MVP. Everything works as it should!

--- team-builder/src/components/App.js
+++ team-builder/src/components/App.js
@@ -1,9 +1,4 @@
-import React, { useState, useEffect } from 'react'
-import { render } from 'react-dom'
-import TeamMember from './TeamMember'
-import TeamForm from './TeamForm'
-// import axios from '../axios'
-import logo from '../logo.svg';
+import React, { useState } from 'react'
 import '../App.css';
 
 const teamList = [
@@ -20,36 +15,8 @@ const initialFormValues = {
 }
 
 export default function SimpleForm() {
-  const [team, setTeam] = useState(teamList)
   const [formValues, setFormValues] = useState(initialFormValues);
-  const [error, setError] = useState("");
-
-  // const updateForm = (inputName, inputValue) => {
-  //   setFormValues({ ...formValues, [inputName]: inputValue });
-  // }
-
-  // const submitForm = () => {
-  //   const newMember = {
-  //     name: formValues.name.trim(),
-  //     location: formValues.location.trim(),
-  //     team: formValues.team
-  //   }
-
-  //   if (!newMember.name || !newMember.location || !newMember.team) {
-  //     setError("All fields are required");
-  //   } else {
-  //     axios.post("http://fakeapi.com", newMember)
-  //       .then(res => {
-  //         const memberFromServer = res.data;
-  //         setTeam([ memberFromServer, ...team ]);
-  //         setFormValues(initialFormValues);
-  //       }).catch(err => console.error(err))
-  //       .finally(() => setError(""))
-  //   }
-  // }
-  // useEffect(() => {
-  //   axios.get('http://fakeapi.com').then(res => setTeam(res.data))
-  // }, [])
+  const [team, setTeam] = useState(teamList);
 
   const change = (evt) => {
     const { value, name } = evt.target;
@@ -59,17 +26,88 @@ export default function SimpleForm() {
     evt.preventDefault();
     const newMember = {
       name: formValues.name.trim(),
+      location: formValues.location.trim(),
+      team: formValues.team
     }
     setTeam(team.concat(newMember));
     setFormValues(initialFormValues);
   }
 
-
-
   return (
     <div className='container'>
       <h1>Form App</h1>
-      <h2>{error}</h2>
+      <form onSubmit={submit}>
+        <input
+          name="name"
+          type="text"
+          placeholder="Name here" 
+          value={formValues.name}
+          onChange={change}
+        />
+        <select value={formValues.location} name="location" onChange={change}>
+          <option value="">-- Select a Location--</option>
+          <option value="AL">AL</option>
+          <option value="AK">AK</option>
+          <option value="AZ">AZ</option>
+          <option value="AR">AR</option>
+          <option value="CA">CA</option>
+          <option value="CO">CO</option>
+          <option value="CT">CT</option>
+          <option value="DE">DE</option>
+          <option value="DC">DC</option>
+          <option value="FL">FL</option>
+          <option value="GA">GA</option>
+          <option value="HI">HI</option>
+          <option value="ID">ID</option>
+          <option value="IL">IL</option>
+          <option value="IN">IN</option>
+          <option value="IA">IA</option>
+          <option value="KS">KS</option>
+          <option value="KY">KY</option>
+          <option value="LA">LA</option>
+          <option value="ME">ME</option>
+          <option value="MD">MD</option>
+          <option value="MA">MA</option>
+          <option value="MI">MI</option>
+          <option value="MN">MN</option>
+          <option value="MS">MS</option>
+          <option value="MO">MO</option>
+          <option value="MT">MT</option>
+          <option value="NE">NE</option>
+          <option value="NV">NV</option>
+          <option value="NH">NH</option>
+          <option value="NJ">NJ</option>
+          <option value="NM">NM</option>
+          <option value="NY">NY</option>
+          <option value="NC">NC</option>
+          <option value="ND">ND</option>
+          <option value="OH">OH</option>
+          <option value="OK">OK</option>
+          <option value="OR">OR</option>
+          <option value="PA">PA</option>
+          <option value="PR">PR</option>
+          <option value="RI">RI</option>
+          <option value="SC">SC</option>
+          <option value="SD">SD</option>
+          <option value="TN">TN</option>
+          <option value="TX">TX</option>
+          <option value="UT">UT</option>
+          <option value="VT">VT</option>
+          <option value="VA">VA</option>
+          <option value="WA">WA</option>
+          <option value="WV">WV</option>
+          <option value="WI">WI</option>
+          <option value="WY">WY</option>
+        </select>
+        <select value={formValues.team} name="team" onChange={change}>
+          <option value="">-- Select a Team --</option>
+          <option value="A">Team A</option>
+          <option value="B">Team B</option>
+          <option value="C">Team C</option>
+          <option value="D">Team D</option>
+        </select>
+        <button>submit</button>
+      </form>
       {team.map((member, idx) => (
         <div key={idx} details={member}>
           <div className='team member container'>
@@ -79,45 +117,6 @@ export default function SimpleForm() {
           </div>
         </div>
       ))}
-      <form onSubmit={submit}>
-        <input 
-          value={formValues.name}
-          onChange={change}
-          name="name"
-          type="text"
-        />
-        <input 
-          value={formValues.location}
-          onChange={change}
-          name="location"
-          type="text"
-        />
-        <input 
-          value={formValues.team}
-          onChange={change}
-          name="team"
-          type="text"
-        />
-        <button>submit</button>
-      </form>
-        
-
-
-      {/* <h1>Form App</h1>
-      <h2>{error}</h2>
-      <TeamForm
-        values={formValues}
-        update={updateForm}
-        submit={submitForm}
-      />
-
-      {
-        team.map((member, idx) => {
-          return (
-            <TeamMember key={idx} details={member} />
-          )
-        })
-      } */}
     </div>
   )
 }
